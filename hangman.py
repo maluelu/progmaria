7f8f1a6ba3f6f25a0f275f977a674835341dbd41
Maria Luisa Neumann <neumannm635@gmail.com>
2023-08-27 18:24:59 -0300

    f

--- hangman.py
+++ hangman.py
@@ -5,7 +5,7 @@ import string
 
 
 def get_valid_word(words):
-    word = random.choice(words)  # randomly chooses something from the list
+    word = random.choice(words) 
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
@@ -14,19 +14,19 @@ def get_valid_word(words):
 
 def hangman():
     word = get_valid_word(words)
-    word_letters = set(word)  # letters in the word
+    word_letters = set(word)  
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what the user has guessed
+    used_letters = set() 
 
     lives = 7
 
-    # getting user input
+
     while len(word_letters) > 0 and lives > 0:
-        # letters used
-        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+ 
+    
         print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-        # what current word is (ie W - R D)
+  
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
@@ -48,7 +48,7 @@ def hangman():
         else:
             print('\nThat is not a valid letter.')
 
-    # gets here when len(word_letters) == 0 OR when lives == 0
+
     if lives == 0:
         print(lives_visual_dict[lives])
         print('Parabens', word)
